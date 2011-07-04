2cfffbcb4e996167a9c7d5b442eda94cdff45d6c
Paolo Agostinelli <paolo.agostinelli@me.com>
2011-07-04 14:29:46 -0400

    Use @print media for print styles and various fixes

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -8,12 +8,11 @@
 <article id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <header><h2<?php print $title_attributes; ?>><?php print $title; ?></h2></header>
+    <?php if ($block->subject): ?>
+      <header><h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2></header>
     <?php endif;?>
   <?php print render($title_suffix); ?>
 
   <?php print $content ?>
 
 </article>
-
